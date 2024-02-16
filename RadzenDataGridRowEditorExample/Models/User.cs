d3068fedaa26d4762a4f6cbe7bb74b7cdff29a08
Malov Gleb <melkitasit@yandex.ru>
2024-02-16 11:59:05 +0500

    init

---
+++ RadzenDataGridRowEditorExample/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace RadzenDataGridRowEditorExample.Models;
+
+public record User
+{
+    public User(string Name, string Surname, int Age)
+    {
+        this.Name = Name;
+        this.Surname = Surname;
+        this.Age = Age;
+    }
+
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public int Age { get; set; }
+}
